49934e53e1dc7f70ec12e9074744d3fac12a44ce
MinayeAliyeva <7s346pu@code.edu.az>
2024-08-30 19:11:30 +0400

    refactor(refactor/time-zone):Drawer sepereted another file

---
+++ my-app/src/components/TimeDrawer.tsx
@@ -0,0 +1,54 @@
+import { Box, Drawer, List, ListItem, Typography } from "@mui/material";
+import React from "react";
+import { timeZones } from "../constants";
+
+const TimeDrawer = ({ drawerOpen, toggleDrawer, handleTimeZoneClick }: any) => {
+  return (
+    <Drawer
+      anchor="bottom"
+      open={drawerOpen}
+      onClose={toggleDrawer(false)}
+      sx={{
+        "& .MuiDrawer-paper": {
+          borderRadius: "16px 16px 0 0",
+          padding: 2,
+          backgroundColor: "#fff",
+          boxShadow: "0px -4px 16px rgba(0, 0, 0, 0.2)",
+        },
+      }}
+    >
+      <Box
+        sx={{
+          width: 300,
+          display: "flex",
+          flexDirection: "column",
+          gap: 1,
+        }}
+      >
+        <List sx={{ padding: 0 }}>
+          {timeZones?.map((timeZone) => (
+            <ListItem
+              key={timeZone?.value}
+              onClick={() =>
+                handleTimeZoneClick(timeZone?.value, timeZone?.value)
+              }
+              sx={{
+                paddingY: 1,
+                borderRadius: "8px",
+                "&:hover": {
+                  backgroundColor: "#f0f0f0",
+                },
+              }}
+            >
+              <Typography sx={{ fontWeight: "bold", fontSize: 16 }}>
+                {timeZone?.value}
+              </Typography>
+            </ListItem>
+          ))}
+        </List>
+      </Box>
+    </Drawer>
+  );
+};
+
+export default TimeDrawer;
