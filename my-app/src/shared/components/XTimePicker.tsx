a0f4814739d31ceefb27441a595242f6b30917c2
MinayeAliyeva <7s346pu@code.edu.az>
2024-09-11 11:57:03 +0400

    bugfix

--- my-app/src/shared/components/XTimePicker.tsx
+++ my-app/src/shared/components/XTimePicker.tsx
@@ -58,27 +58,35 @@ const XTimePicker = ({
     setSound(e.target.value as string);
   };
 
+
   const handleAccept = () => {
     const formattedTime = formatTime(time);
     const formattedDate = formatDate(time);
-    const now = new Date();
-    const alarmTime = getAlarmTimeDetails(formattedTime, formattedDate);
-    const timeDiff = alarmTime.getTime() - now.getTime();
-    dispatch(
-      setAlarm({
-        date: formattedDate,
-        time: formattedTime,
-        note: note,
-        isActive: true,
-        sound: sound ? `/sounds/${sound}` : "/sounds/alarm1.mp3",
-        id: uid(),
-        isPastTime: timeDiff < 0,
-      })
-    );
-    setNote("");
-    setTime(new Date());
-    setSound("");
-    onCloseDrawer(false);
+    
+    const dateParts = formattedDate.split("-");
+    if (dateParts[2]?.length === 2 && dateParts[1]?.length === 2 && dateParts[0]?.length === 4) {
+      const now = new Date();
+      const alarmTime = getAlarmTimeDetails(formattedTime, formattedDate);
+      const timeDiff = alarmTime.getTime() - now.getTime();
+      
+      dispatch(
+        setAlarm({
+          date: formattedDate,
+          time: formattedTime,
+          note: note,
+          isActive: true,
+          sound: sound ? `/sounds/${sound}` : "/sounds/alarm1.mp3",
+          id: uid(),
+          isPastTime: timeDiff < 0,
+        })
+      );
+      setNote("");
+      setTime(new Date());
+      setSound("");
+      onCloseDrawer(false);
+    } else {
+  
+    }
   };
 
   return (
